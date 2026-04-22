6483248c3f44755b9a25f1e5a9a46be2ae12a9c2
Renzo Rouma <renzo.rouma@gmail.com>
2026-04-22 23:44:00 +0200

    added caching to requests

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import { loadEnvFile } from "node:process";
 import type { ClassResource, Resource, SchoolData, SchoolYear, Timetable } from "./types.ts";
+import NodeFetchCache, { FileSystemCache, MemoryCache } from "node-fetch-cache";
 
 loadEnvFile();
 const baseUrl: string = process.env.APIBASEURL;
@@ -9,20 +10,33 @@ function newURL(path: string, params?: URLSearchParams): string {
     return `${baseUrl}${path}` + (params ? `?${params}` : "");
 }
 
+function createFetchCache(time: { days?: number; hours?: number; minutes?: number }) {
+    const days: number = time.days ?? 0;
+    const hours: number = time.hours ?? 0;
+    const minutes: number = time.minutes ?? 0;
+
+    return NodeFetchCache.create({
+        shouldCacheResponse: (response) => response.ok,
+        cache: new FileSystemCache({ cacheDirectory: "./cache", ttl: days * 86400 + hours * 3600 + minutes * 60 }),
+    });
+}
+
 export async function getClasses(): Promise<ClassResource[]> {
     const params = new URLSearchParams({
         resourceType: "CLASS",
     });
 
-    const response = await fetch(newURL("/timetable/filter", params), { headers: headers });
-    const data: Resource = await response.json();
+    const fetchCache = createFetchCache({ days: 7 });
+    const response = await fetchCache(newURL("/timetable/filter", params), { headers: headers });
+    const data = (await response.json()) as Resource;
 
     return data.classes;
 }
 
 export async function getCurrentSchoolyear(): Promise<SchoolYear> {
-    const response = await fetch(newURL("/app/data"), { headers: headers });
-    const data: SchoolData = await response.json();
+    const fetchCache = createFetchCache({ days: 7 });
+    const response = await fetchCache(newURL("/app/data"), { headers: headers });
+    const data = (await response.json()) as SchoolData;
 
     return data.currentSchoolYear;
 }
@@ -36,8 +50,9 @@ export async function getTimetable(classId: number): Promise<Timetable> {
         resources: classId.toString(),
     });
 
-    const response = await fetch(newURL("/timetable/entries", params), { headers: headers });
-    const data: Timetable = await response.json();
+    const fetchCache = createFetchCache({ minutes: 15 });
+    const response = await fetchCache(newURL("/timetable/entries", params), { headers: headers });
+    const data = (await response.json()) as Timetable;
 
     return data;
 }
