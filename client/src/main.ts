28780d2ce338199fc5ddfcd489214076c056e501
Renzo Rouma <renzo.rouma@gmail.com>
2026-05-26 22:19:59 +0200

    added find functionality

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,23 @@ import "./style.css";
 import type { Class } from "./types";
 
 async function main() {
+    const form: HTMLFormElement = document.querySelector("#find")!;
+    const findTextRef: HTMLInputElement = form.querySelector("#find-text")!;
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const query: string = findTextRef.value;
+
+        if ("find" in window && typeof (window as any).find === "function") {
+            (window as any).find(query);
+        } else {
+            // Fallback for browsers that don't support window.find()
+            alert(
+                "window.find() is not supported in this browser.\n\nUse your browsers built-in search functionality ('ctrl+f' or 'Find in page') instead."
+            );
+        }
+    });
+
     const classes: Class[] = await get("/classes");
     const classesListRef: HTMLElement = document.querySelector("div#classes")!;
 
