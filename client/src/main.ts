f4efede6761d19fa3717525030f8adb00749e20c
Viovyx <renzo.rouma@gmail.com>
2026-08-19 16:45:02 +0200

    added client side support for custom schoolyears (dateranges)

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,59 @@
 import "./style.css";
-import type { Class } from "./types";
+import type { Class, DateRange, SchoolYear } from "./types";
 
-async function main() {
+loadClasses();
+loadSchoolyears();
+initSearch();
+
+async function loadClasses(schoolyearId?: number) {
+    const classesListRef: HTMLElement = document.querySelector("div#classes")!;
+    classesListRef.innerHTML = "";
+
+    let classes: Class[] = [];
+    let dateRange: DateRange;
+    if (schoolyearId) {
+        const schoolyears: SchoolYear[] = await get("/schoolyears");
+        dateRange = schoolyears.find(
+            (schoolyear) => schoolyear.id === schoolyearId
+        )!.dateRange;
+        classes = await get(
+            `/classes?start=${dateRange.start}&end=${dateRange.end}`
+        );
+    } else classes = await get("/classes");
+
+    classes.forEach((classData) => {
+        const classEl = document.createElement("div");
+        classEl.innerHTML = `<p>${classData.name}</p><span>${classData.id}</span>`;
+        classEl.id = String(classData.id);
+        classEl.addEventListener("click", (e) => {
+            const el = e.target as HTMLElement;
+            if (schoolyearId) copy(el.id, dateRange);
+            else copy(el.id);
+        });
+        classesListRef.appendChild(classEl);
+    });
+}
+
+async function loadSchoolyears() {
+    const schoolyearsSelectRef: HTMLSelectElement =
+        document.querySelector("select#schoolyears")!;
+    const schoolyears: SchoolYear[] = await get("/schoolyears");
+
+    schoolyearsSelectRef.innerHTML = `<option value="" selected>Current Schoolyear</option>`;
+    schoolyearsSelectRef.innerHTML += schoolyears
+        .map(
+            (schoolyear) =>
+                `<option value="${schoolyear.id}">${schoolyear.name}</option>`
+        )
+        .join("");
+
+    schoolyearsSelectRef.addEventListener("change", (e) => {
+        const selected = (e.target as HTMLSelectElement).selectedOptions[0];
+        loadClasses(+selected.value);
+    });
+}
+
+function initSearch() {
     const form: HTMLFormElement = document.querySelector("#find")!;
     const findTextRef: HTMLInputElement = form.querySelector("#find-text")!;
 
@@ -18,34 +70,22 @@ async function main() {
             );
         }
     });
-
-    const classes: Class[] = await get("/classes");
-    const classesListRef: HTMLElement = document.querySelector("div#classes")!;
-
-    classes.forEach((classData) => {
-        const classEl: HTMLElement = document.createElement("div");
-        classEl.innerHTML = `<p>${classData.name}</p><span>${classData.id}</span>`;
-        classEl.id = String(classData.id);
-        classEl.addEventListener("click", (e) => {
-            const el: HTMLElement = e.target as HTMLElement;
-            copy(el.id);
-        });
-        classesListRef.appendChild(classEl);
-    });
 }
 
+//#region Helper functions
 async function get(path: string, method: string = "get") {
     const response = await fetch(path, { method: method });
     const data = response.json();
     return data;
 }
 
-function copy(id: string) {
-    const url = location.href + "calendar?class=" + id;
+function copy(id: string, dateRange?: DateRange) {
+    let url = `${location.href}calendar?class=${id}`;
+    if (dateRange) url += `&start=${dateRange.start}&end=${dateRange.end}`;
+
     navigator.clipboard.writeText(url);
     alert(
         `${url} copied to clipboard!\nPaste it in your calendar app to sync.`
     );
 }
-
-main();
+//#endregion
--- client/src/types.ts
+++ client/src/types.ts
@@ -2,3 +2,14 @@ export interface Class {
     id: number;
     name: string;
 }
+
+export interface SchoolYear {
+    dateRange: DateRange;
+    id: number;
+    name: string;
+}
+
+export interface DateRange {
+    start: string;
+    end: string;
+}
