b736a52554af1f1db80b5d40d2606dafea5a1e04
Viovyx <renzo.rouma@gmail.com>
2026-09-20 00:30:06 +0200

    added icons to event details

--- client/src/ts/calview.ts
+++ client/src/ts/calview.ts
@@ -131,8 +131,14 @@ const calendar = new Calendar(calendarEl, {
             content: `
                 <p class="time">${info.event.start.toLocaleString("nl-BE", { dateStyle: "short", timeStyle: "short" })} - ${info.event.end.toLocaleString("nl-BE", { timeStyle: "short" })}</p>
                 <h3>${info.event.title}</h3>
-                <p>${info.event.extendedProps.location}</p>
-                <p>${(info.event.extendedProps.description as string).replaceAll("\n", "</br>")}</p>
+                <div class="event-detail-group">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M536.5-503.5Q560-527 560-560t-23.5-56.5Q513-640 480-640t-56.5 23.5Q400-593 400-560t23.5 56.5Q447-480 480-480t56.5-23.5ZM480-186q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/></svg>
+                    <p>${info.event.extendedProps.location}</p>
+                </div>
+                <div class="event-detail-group">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-280h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Zm-80 480q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg>
+                    <p>${(info.event.extendedProps.description as string).replaceAll("\n", "</br>")}</p>
+                </div>
             `
         })
 });
